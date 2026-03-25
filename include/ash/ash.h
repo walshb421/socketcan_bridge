cf3034b06658f420a167d2faa0a02114254377f7
deckhand-agent <deckhand@deckhand.dev>
2026-03-24 20:23:25 -0700

    Add project scaffold with CMake build system and stub sources (#16)

---
+++ include/ash/ash.h
@@ -0,0 +1,16 @@
+#ifndef ASH_H
+#define ASH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ash - CAN bridge client library */
+
+typedef struct ash_ctx ash_ctx_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASH_H */
---
+++ server/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("ash-server starting\n");
+    return 0;
+}
